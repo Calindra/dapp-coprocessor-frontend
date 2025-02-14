f1b3ff331b3892a8f2bef0d18012d2ae5d923346
Fabio Oshiro <fabio.oshiro@calindra.com.br>
2025-02-14 17:19:37 -0300

    fix: handling the missing window.ethereum

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,14 +6,16 @@ import SoccerTeamManager from './views/SoccerTeamManager'
 
 function App() {
   const [address, setAddress] = useState(localStorage.getItem("connectedAccount") ?? '')
-  window.ethereum.on("accountsChanged", (accounts: string[]) => {
-    if (accounts.length > 0) {
-      setAddress(accounts[0])
-      console.log(`address`, accounts[0])
-    } else {
-      setAddress('')
-    }
-  });
+  if (typeof window.ethereum !== 'undefined') {
+    window.ethereum.on("accountsChanged", (accounts: string[]) => {
+      if (accounts.length > 0) {
+        setAddress(accounts[0])
+        console.log(`address`, accounts[0])
+      } else {
+        setAddress('')
+      }
+    });
+  }
   return (
     <>
       <Button onClick={connectMetaMask}>{address ? `Connected: ${address.substring(0, 7)}...${address.substring(address.length - 5)}` : 'Connect'}</Button>
